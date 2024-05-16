1a9e1d4d9378b44b067af98d46bbeda769870e77
argo79 <melloni6@gmail.com>
2024-05-16 23:14:13 +0200

    modifica welcome

--- ui/welcome.c
+++ ui/welcome.c
@@ -87,7 +87,7 @@ void UI_DisplayWelcome(void)
 		UI_PrintString(WelcomeString0, 0, 127, 0, 10);
 		UI_PrintString(WelcomeString1, 0, 127, 2, 10);
 		UI_PrintString(WelcomeString2, 0, 127, 4, 10);
-		UI_PrintStringSmall(Version, 2, 127, 6);
+		UI_PrintStringSmallBold(Version, 2, 127, 6);
 
 		ST7565_BlitStatusLine();  // blank status line
 		ST7565_BlitFullScreen();
